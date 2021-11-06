9d396e04f7360388a57a243058e644190358fc31
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2021-11-06 06:51:48 +0400

    добавил ашан, поправил подбор аган, ртк, электротехмонтаж, поправил подбор даты

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # Модуль работы с ИТКО (выгрузка  отчетов "сформировать")
 # pip install pyautogui
 # разрешение 2560 х 1440, 150% маштаб
-
+import os
 from datetime import datetime
 import pyautogui as pg
 import subprocess
 import configparser
 import time
+import glob
 
 ITKO_BIN = 'C:/1Cv77/BIN/1cv7.exe'
 
 cfg = configparser.ConfigParser()  # создание объекта с вызовом класса модуля работы с .ini файлами
 cfg.read('config.ini')
+export_dir = cfg.get('PATHS', 'dir')
+NAME_0 = cfg.get('NAMES', 'ашан')
 NAME_1 = cfg.get('NAMES', 'РТК')
 NAME_2 = cfg.get('NAMES', 'электротехмонтаж')
 NAME_3 = cfg.get('NAMES', 'автопитер')
@@ -22,7 +25,7 @@ NAME_7 = cfg.get('NAMES', 'мегафон')
 NAME_8 = cfg.get('NAMES', 'мэлон')
 NAME_9 = cfg.get('NAMES', 'сапв')
 
-NAMES = [NAME_1, NAME_2, NAME_3, NAME_4, NAME_5, NAME_6, NAME_7, NAME_8, NAME_9]  # лист с контрагентами
+NAMES = [NAME_0, NAME_1, NAME_2, NAME_3, NAME_4, NAME_5, NAME_6, NAME_7, NAME_8, NAME_9]  # лист с контрагентами
 
 
 # Функции
@@ -48,12 +51,16 @@ def past_dates(year=21):
         if past_month in [4, 6, 9, 11]:  # апрель, июнь, сентябрь, ноябрь
             if len(str(past_month)) == 1:  # если месяц из 1 символа то конкатенация с '0'
                 past_month_str = '0' + str(past_month)  # апрель, июнь, сентябрь
+            else:
+                past_month_str = str(past_month)
             past_start_date_ = '01' + f'.{past_month_str}.{year}'
             past_finish_date_ = '30' + f'.{past_month_str}.{year}'
 
         elif past_month in [1, 3, 5, 7, 8, 10]:  # январь, март, май, июль, август, октябрь
             if len(str(past_month)) == 1:  # если месяц из 1 символа то конкатенация с '0'
                 past_month_str = '0' + str(past_month)  # январь, март, май, июль, август
+            else:
+                past_month_str = str(past_month)
             past_start_date_ = '01' + f'.{past_month_str}.{year}'
             past_finish_date_ = '31' + f'.{past_month_str}.{year}'
 
@@ -74,9 +81,16 @@ def welcoming(name_='ИТКО', author_='Вячеслав Митин', version_=
     print(f"Версия модуля: '{version_}'\n")
 
 
+def cleaning_export_dir():
+    """Функция удаления файлов эксель в каталоге экспорта"""
+    for files in glob.glob(export_dir + '*.xls'):
+        os.remove(files)
+    time.sleep(0.5)
+
+
 def start_itko():
     """Функция запуска 1С 7 ИТКО"""
-    starting = subprocess.Popen([
+    subprocess.Popen([
         ITKO_BIN
     ])
 
@@ -113,6 +127,8 @@ def searching_exporting(name):
     pg.press('pageup')
     time.sleep(1)
     pg.write(name)
+    if name == NAME_0 or name == NAME_1 or name == NAME_2:  # дополнительный поиск 'Ашан', 'РТК' и 'электротехмонтаж'
+        pg.hotkey('shift', 'F3')
     pg.hotkey('shift', 'F3')
     pg.press('enter')
     pg.press('tab')
@@ -151,6 +167,7 @@ def cycling_exports():
 
 if __name__ == '__main__':
     welcoming()
+    cleaning_export_dir()
     start_itko()
     cycling_exports()
     pg.hotkey('alt', 'F4')

0609a0153aa20de4638c60d95173ee09ead200cf
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2021-12-31 03:07:51 +0400

    рефакторинг, доработки меню и входа

--- main.py
+++ main.py
@@ -19,13 +19,23 @@ def welcoming(name_='ИТКО', author_='Вячеслав Митин', version_=
     print(f"Версия модуля: '{version_}'\n")
 
 
-def start_itko_bookkeeper():
+def start_itko(point='buh'):
     """Функция запуска 1С 7 ИТКО"""
     subprocess.Popen([
         ITKO_BIN
     ])
 
     time.sleep(2)
+    pg.press('tab', presses=2)
+    pg.press('home')  # выбор первой базы в списке баз
+    pg.press('enter')
+    pg.press('home')  # выбор администратора для точки отсчета
+
+    if point == 'buh':  # выбор бухгалтера
+        pg.press('down', presses=7)
+    elif point == 'adm':  # оставить администратора
+        pass
+
     pg.press('enter', presses=4, interval=0.5)
     pg.press('tab', presses=2, interval=0.5)
     pg.press('enter')
@@ -35,9 +45,10 @@ def pyautogui_menu() -> str:
     """Функция МЕНЮ"""
     menu_points = {
         0: 'Загрузка базы ИТКО',
-        1: 'Старт ИТКО',
+        1: 'Старт ИТКО Бухгалтером',
         2: "Файлы 'Сформировать.xls'",
         3: "Подготовка к формированию ВОУ",
+        4: 'Старт ИТКО Администратором',
     }
 
     return pg.prompt(text=f"""
@@ -47,6 +58,8 @@ def pyautogui_menu() -> str:
     1: {menu_points.get(1)}
     2: {menu_points.get(2)}
     3: {menu_points.get(3)}
+    4: {menu_points.get(4)}
+
     """, title='МЕНЮ АВТОМАТИЗАЦИИ ИТКО', default='1')
 
 
@@ -56,13 +69,15 @@ if __name__ == '__main__':
     if select == '0':
         pass
     elif select == '1':
-        start_itko_bookkeeper()
+        start_itko()
     elif select == '2':
         from MyModules.exporting_xls import *
-        start_itko_bookkeeper()
+        start_itko(point='buh')
         cleaning_export_dir()
         cycling_exports()
         make_separator()
         quit_1c()
     elif select == '3':
         pass
+    elif select == '4':
+        start_itko(point='adm')

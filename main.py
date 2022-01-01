1c75d5de6ffb0d0ef52c864d625b7ecf62a016ac
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2022-01-01 09:02:24 +0400

    рефакторинг, доработки экспортов

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 # ИМПОРТЫ
 import os
-
-import pyautogui
 import pyautogui as pg
 import time
 from MyModules.typing_unicode_str import typing_unicode_str as typing
@@ -144,24 +142,77 @@ def export_vskk():
 
 
 def export_vou():
-    print_log("Экспорт ВОУ", line_before=True)
+    print_log("Работа с ВОУ", line_before=True)
+    import pyperclip
     from MyModules.past_dates import past_dates
 
+    print_log("Экспорт ВОУ в текстовый файл")
     pg.click(885, 85)  # экспорт файла
     pg.press('tab', presses=9, interval=0.2)
     pg.write(past_dates()[1])
     pg.press('tab')
     pg.write(past_dates()[0])
     pg.press('tab')
-    typing(PATH_VOU + f'VOU OKO')
+    typing(PATH_VOU + f'VOU_OKO.txt')
     pg.press('tab')
     pg.press('enter')
     time.sleep(3)
 
+    print_log("Генерация XLS файла")
     pg.click(785, 85)  # вызов журнала с ведомостями
+    if past_dates()[3] == '12':
+        pg.press('alt')
+        pg.press('right', presses=1, interval=0.1)
+        pg.press('down', presses=12, interval=0.1)
+        pg.press('enter')
 
+        def clear_dates():
+            pg.press('right', presses=10, interval=0.1)
+            pg.press('backspace', presses=10, interval=0.1)
+
+        clear_dates()
+        typing(past_dates()[0])
+        pg.press('tab')
+        clear_dates()
+        typing(past_dates()[1])
+        pg.press('tab')
+        pg.press('enter')
+        # input('')
+    pg.press('end')
+    pg.press('enter')
+    pg.press('tab', presses=4, interval=0.2)
+    pg.hotkey('ctrl', 'c')
+    pg.press('tab', presses=12, interval=0.2)
+    pg.press('space')
+    pg.press('down')
+    pg.press('enter')
+    time.sleep(4)
+    pg.hotkey('ctrl', 's')
+    time.sleep(0.5)
+    typing(PATH_VOU + f'VOU_OKO_{pyperclip.paste()}')
+    pg.press('tab')
+    pg.press('down', presses=2)  # выбор формата файла
+    pg.press('enter', presses=2)  # сохранение файла
+    time.sleep(0.5)
+
+    print_log("Генерация DBF файла")
     pg.click(1025, 85)  # открытие обработки для превращения в ДБФ
-
+    pg.press('f4')
+    time.sleep(0.5)
+    typing(f'VOU_OKO_{past_dates()[0]}_{past_dates()[1]}.txt')
+    time.sleep(0.5)
+    pg.press('enter')
+    time.sleep(0.5)
+    pg.click(10, 680)
+    pg.hotkey('shift', 'tab')
+    typing(past_dates()[1])
+    pg.press('tab', presses=2, interval=0.2)
+    pg.press('enter')
+    time.sleep(30)
+    pg.press('tab')
+    time.sleep(0.5)
+    pg.press('enter')
+    time.sleep(3)
 
 
 def pyautogui_menu() -> str:
@@ -172,7 +223,7 @@ def pyautogui_menu() -> str:
         0: 'Загрузка базы ИТКО',
         1: 'Старт ИТКО Бухгалтером',
         2: 'Старт ИТКО Администратором',
-    3: "Подготовка к формированию ВОУ",
+        3: "Подготовка к формированию ВОУ",
         4: "Файлы 'Сформировать.xls'",
         5: "Выгрузка '014'",
         6: "Выгрузка 'ВСКК'",
@@ -196,7 +247,7 @@ def pyautogui_menu() -> str:
     7: {menu_points.get(7)}
     =========================
     9: {menu_points.get(9)}
-    """, title='МЕНЮ АВТОМАТИЗАЦИИ ИТКО', default='1')
+    """, title='МЕНЮ АВТОМАТИЗАЦИИ ИТКО', default='7')
 
 
 if __name__ == '__main__':
@@ -238,7 +289,7 @@ if __name__ == '__main__':
     elif select == '7':
         start_itko(point='buh')
         export_vou()
-        # quit_1c()
+        quit_1c()
 
     elif select == '9':
         change_datetime()
